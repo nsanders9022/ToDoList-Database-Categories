732e0ae41fe53c676710465aac43a38cafb5a155
Katy Daviscourt and Kory Skarbek <katy+kory@>
2017-02-20 10:43:17 -0800

    pass first three tests for database empty at first, returns true if descriptions are the same, and saves to database.

---
+++ Objects/Task.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace ToDoList
+{
+  public class Task
+  {
+    private int _id;
+    private string _description;
+
+    public Task(string Description, int Id = 0)
+    {
+      _id = Id;
+      _description = Description;
+    }
+
+    public override bool Equals(System.Object otherTask)
+    {
+      if (!(otherTask is Task))
+      {
+        return false;
+      }
+      else
+      {
+        Task newTask = (Task) otherTask;
+        bool descriptionEquality = (this.GetDescription() == newTask.GetDescription());
+        return (descriptionEquality);
+      }
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetDescription()
+    {
+      return _description;
+    }
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+    public static List<Task> GetAll()
+    {
+      List<Task> allTasks = new List<Task>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM tasks;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int taskId = rdr.GetInt32(0);
+        string taskDescription = rdr.GetString(1);
+        Task newTask = new Task(taskDescription, taskId);
+        allTasks.Add(newTask);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allTasks;
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO tasks (description) OUTPUT INSERTED.id VALUES (@TaskDescription);", conn);
+
+      SqlParameter descriptionParameter = new SqlParameter();
+      descriptionParameter.ParameterName = "@TaskDescription";
+      descriptionParameter.Value = this.GetDescription();
+      cmd.Parameters.Add(descriptionParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM tasks;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+  }
+}
---
+++ Tests/TaskTest.cs
@@ -0,0 +1,57 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ToDoList
+{
+  public class ToDoTest : IDisposable
+  {
+    public ToDoTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=todo_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test_DatabaseEmptyAtFirst()
+    {
+     //Arrange, Act
+     int result = Task.GetAll().Count;
+
+     //Assert
+     Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Test_Equal_ReturnsTrueIfDescriptionsAreTheSame()
+    {
+      //Arrange, Act
+      Task firstTask = new Task("Mow the lawn");
+      Task secondTask = new Task("Mow the lawn");
+
+      //Assert
+      Assert.Equal(firstTask, secondTask);
+    }
+
+    [Fact]
+    public void Test_Save_SavesToDatabase()
+    {
+      //Arrange
+      Task testTask = new Task("Mow the lawn");
+
+      //Act
+      testTask.Save();
+      List<Task> result = Task.GetAll();
+      List<Task> testList = new List<Task>{testTask};
+
+      //Assert
+      Assert.Equal(testList, result);
+    }
+
+    public void Dispose()
+    {
+      Task.DeleteAll();
+    }
+  }
+}
